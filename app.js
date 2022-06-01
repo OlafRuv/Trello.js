831a5fd060241e9f67e2bf6cfa7d2026d3a1cc1c
Olaf <olaf.ruvag@gmail.com>
2022-05-31 22:33:11 -0500

    Hacemos uso de la API de trello empaquetada para node.js

--- app.js
+++ app.js
@@ -1,5 +1,57 @@
-require('dotenv').config()
+const Trello = require("./main");
+var trello = new Trello("key", "token");
+var cardTitle = `Card Nueva ${new Date()}`;
 
-if(!process.env.TOKEN && !process.env.KEY){
-  throw new Error('No hay configuración con Api Key y con Token')
-}
+console.log("Ejecutando!");
+
+trello.addCard(cardTitle, "LaunchX Card Description", "IdList",
+  function (error, trelloCard) {
+    if (error) {
+      console.log('Could not add card:', error);
+    }
+    else {
+      console.log('Added card:', trelloCard);
+    }
+  });
+// require('dotenv').config()
+
+// if(!process.env.TOKEN && !process.env.KEY){
+//   throw new Error('No hay configuración con Api Key y con Token')
+// }
+
+// let Trello = require("trello");
+// let trello = new Trello(process.env.KEY, process.env.TOKEN);
+
+// let cardTitle = `Card Nueva ${new Date()}`
+
+// trello.addCard(cardTitle, "LaunchX Card Description", "627eb22d7bb08a78b5314235",
+// 	function (error, trelloCard) {
+// 		if (error) {
+// 			console.log('Could not add card:', error);
+// 		}
+// 		else {
+// 			console.log('Added card:', trelloCard);
+// 		}
+// 	});
+
+// trello.addLabelToCard("6296d31df223560be36237d0", cover,
+//     function (error, label) {
+//         if (error) {
+//             console.log('Could not add label:', error);
+//         }
+//         else {
+//             console.log('Added label:', label);
+//         }
+//     }
+// )
+
+// trello.addLabelOnBoard("6296d31df223560be36237d0", label1,
+//     function (error, label) {
+//         if (error) {
+//             console.log('Could not add label:', error);
+//         }
+//         else {
+//             console.log('Added label:', label);
+//         }
+//     }
+// )
